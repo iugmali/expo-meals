51d65a1fca316787a132a45907595d8b97259ead
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-06-06 22:52:10 -0300

    add ingredients and steps to meal details

--- src/screens/MealDetails.js
+++ src/screens/MealDetails.js
@@ -1,17 +1,40 @@
-import {StyleSheet, View} from "react-native";
+import {Platform, ScrollView, StyleSheet, Text, View} from "react-native";
 import {MEALS} from "../data/dummy-data";
 import {useLayoutEffect} from "react";
 
 const MealDetails = ({route, navigation}) => {
-  const meal = MEALS.find((m) => m.id === route.params.mealId)
+  const {id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree} = MEALS.find((m) => m.id === route.params.mealId)
+
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: meal.title
+      title: title
     })
-  }, [meal])
+  }, [title])
+
   return (
     <View style={styles.container}>
-
+      <ScrollView>
+        <Text style={styles.sectionTitle}>Ingredients</Text>
+        <View>
+          {ingredients.map((ingredient, index) => <Text key={index} style={styles.sectionItem}>- {ingredient}</Text>)}
+        </View>
+        <Text style={styles.sectionTitle}>Steps</Text>
+        <View>
+          {steps.map((step, index) => <Text key={index} style={styles.sectionItem}>{index + 1} - {step}</Text>)}
+        </View>
+      </ScrollView>
     </View>
   )
 }
@@ -21,6 +44,16 @@ export default MealDetails
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15
+    padding: 15,
+    backgroundColor: 'white'
+  },
+  sectionTitle: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 16
+  },
+  sectionItem: {
+
   }
 })
