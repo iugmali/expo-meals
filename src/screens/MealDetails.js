5ebbd3716d2f4a01f11d2c568ba22b3921da6095
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-06-07 15:48:49 -0300

    adjust meal details design

--- src/screens/MealDetails.js
+++ src/screens/MealDetails.js
@@ -1,6 +1,7 @@
-import {Platform, ScrollView, StyleSheet, Text, View} from "react-native";
+import {Image, Platform, Pressable, ScrollView, StyleSheet, Text, View} from "react-native";
 import {MEALS} from "../data/dummy-data";
 import {useLayoutEffect} from "react";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const MealDetails = ({route, navigation}) => {
   const {id,
@@ -19,41 +20,63 @@ const MealDetails = ({route, navigation}) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: title
+      title: title,
+      headerRight: () => (
+          <Pressable>
+            <Ionicons name={"star"} size={24} color={"white"} />
+          </Pressable>
+        )
     })
-  }, [title])
+  }, [navigation, title])
 
   return (
-    <View style={styles.container}>
       <ScrollView>
+        <Image style={styles.image} source={{uri: imageUrl}} />
         <Text style={styles.sectionTitle}>Ingredients</Text>
         <View>
-          {ingredients.map((ingredient, index) => <Text key={index} style={styles.sectionItem}>- {ingredient}</Text>)}
+          {ingredients.map((ingredient, index) =>
+            <View key={index} style={styles.sectionContainer}>
+              <Text style={styles.sectionItem}>- {ingredient}</Text>
+            </View>
+          )}
         </View>
         <Text style={styles.sectionTitle}>Steps</Text>
         <View>
-          {steps.map((step, index) => <Text key={index} style={styles.sectionItem}>{index + 1} - {step}</Text>)}
+          {steps.map((step, index) =>
+            <View key={index} style={styles.sectionContainer}>
+              <Text style={styles.sectionItem}>{index + 1}) {step}</Text>
+            </View>
+          )}
         </View>
       </ScrollView>
-    </View>
   )
 }
 
 export default MealDetails
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 15,
-    backgroundColor: 'white'
+  headerRight: {
+    color: 'white'
+  },
+  image: {
+    width: '100%',
+    height: 250
+  },
+  sectionContainer: {
+    backgroundColor: '#8a766f',
+    marginVertical: 4,
+    marginHorizontal:24,
+    borderRadius: 8,
+    padding: 8
   },
   sectionTitle: {
+    color: 'white',
     textAlign: "center",
     fontSize: 18,
     fontWeight: "bold",
     marginVertical: 16
   },
   sectionItem: {
-
+    color: '#3f2f25'
   }
 })
